0ada34aeb104cbe8cd8fa30472e348b88195cf86
SanjayMohan <sanjaykakkatiri@gmail.com>
2020-09-15 20:39:33 +0530

    master

--- pointers/dynamicarray.cpp
+++ pointers/dynamicarray.cpp
@@ -1,9 +1,22 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
+int main()
+{
     int n;
-    cout<<"\nEnter array size :";
-    cin>>n;
+    cout << "\nEnter array size :";
+    cin >> n;
+    int *p = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        cout<<"Enter element no "<<i+1<<endl;
+        cin>>p[i];
+    }
+    cout<<"\nElements are :";
+    for(int i=0;i<n;i++){
+        cout<<p[i]<<endl;
+    }
+
+    delete p;
 
     return 0;
 }
--- pointers/firstpointer.cpp
+++ pointers/firstpointer.cpp
@@ -1,17 +1,19 @@
 #include<iostream>
 using namespace std;
 int main(){
+    int * ptr;
+    cout<<ptr;
     int var1=11;
     int var2=21;
     cout<<"\nVar 1 address :"<<&var1;
-    cout<<"\nVar 2 address :"<<&var2;
-    int* ptr;
+    cout<<"\nVar 2 address :"<<&var2<<endl;
+    
     ptr=&var1;
     cout<<"\nVar1 ="<<ptr;
     ptr=&var2;
     cout<<"\nVar2 ="<<*ptr;
 
 
-
+    delete ptr;
     return 0;
 }
