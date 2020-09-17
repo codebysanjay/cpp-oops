e207d14ccfac471771c2e8a85b471922bcedd1b6
SanjayMohan <sanjaykakkatiri@gmail.com>
2020-09-18 02:05:24 +0530

    master

--- pointers/alpha.cpp
+++ pointers/alpha.cpp
@@ -18,5 +18,16 @@ class alpha{
         }
 };
 int main(){
-    
+    alpha a1(37);
+    alpha a2;
+    a2=a1;
+    cout<<"\na2 =";a2.display();
+    alpha a3=a2;
+    cout<<"\na3 =";a3.display();
+    alpha a4(a3);
+    cout<<"\na4 =";a4.display();
+    cout<<endl;
+    cout<<endl;return 0;
+
+
 }
